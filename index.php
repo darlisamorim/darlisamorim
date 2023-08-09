695e3e73046e503514c14459142eda7b8c677434
Darlis A. Amorim <eu@darlisalvesamorim.dev>
2023-08-08 22:52:22 -0300

    Update

--- index.php
+++ index.php
@@ -7,8 +7,26 @@ use CoffeeCode\Router\Router;
 
 $router = new Router(DOMAIN);
 
-
-
-
-
+$router->group(null);
+$router->get("/", function ($data){
     echo "<h1>Composer AutoLoad 1</h1>";
+    var_dump($data);
+});
+
+$router->get("/contato", function ($data){
+    echo "<h1>Contato</h1>";
+    var_dump($data);
+});
+
+$router->group("ooooooopps");
+$router->get("/{errcode}", function ($data){
+    echo "<h1>Erro {$data["errcode"]}</h1>";
+    var_dump($data);
+});
+
+
+$router->dispatch();
+
+if($router->error()){
+    $router->redirect("/ooooooopps/{$router->error()}");
+}
